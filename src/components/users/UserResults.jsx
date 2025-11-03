2fed0ff099e67ecb4f38999b59af716d0d646d33
nycwy <stuxy.1129@gmail.com>
2025-11-03 19:57:04 +0530

    GitHub api setup and checked whether it works or not

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+
+function UserResults() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`
+            }
+        });
+        const data = await response.json();
+
+        setUsers(data);
+        setLoading(false);
+    }
+
+    if (!loading) {
+        return <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+            {users.map((user) => {
+                return <h1 key={user.id}>{user.login}</h1>
+            })}
+        </div>;
+    } else {
+        return <Spinner />
+    }
+}
+
+export default UserResults;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import UserResults from "../components/users/UserResults";
+
 function Home() {
-    return <div>
-        <h1 className="text-6xl">Welcome</h1>
-    </div>;
+    return (
+        <div>
+            {/* Search Component */}
+            <UserResults />
+        </div>
+        );
 }
 
 export default Home;
