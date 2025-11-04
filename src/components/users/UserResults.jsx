cd49f3f165741fa9afb6c7f6747b9fe783784547
nycwy <stuxy.1129@gmail.com>
2025-11-04 13:14:13 +0530

    User display properties added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
     const [users, setUsers] = useState([]);
@@ -24,7 +25,7 @@ function UserResults() {
     if (!loading) {
         return <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
             {users.map((user) => {
-                return <h1 key={user.id}>{user.login}</h1>
+                return <UserItem key={user.id} user={ user } />
             })}
         </div>;
     } else {
